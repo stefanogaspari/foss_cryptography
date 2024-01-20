9975ddd77f1303604a463ec92edc2b992574cf0c
stefanogaspari <stefano@stefanogaspari.com>
2024-01-20 15:47:32 +0400

    Released v.1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='foss_cryptography',
     packages=find_packages(include=['cryptography']),
-    version='1.0.0',
+    version='1.0.1',
     description='Elliptic Cryptography',
     author='Stefano',
     setup_requires=['pytest-runner'],
