cd0959ae840b04285a59b79d1ba168d591d888b9
stefanogaspari <stefano@stefanogaspari.com>
2024-01-28 11:20:16 +0300

    Adjust tests modules

--- tests/test_elliptic.py
+++ tests/test_elliptic.py
@@ -1,9 +1,10 @@
 from cryptography.ellipticCurves import EllipticCurve
 from cryptography.curves.secp256k1 import p, a, b, origin_G
-from tests.vectors import privKey, pubKey
+from tests.vectors import n, n_times_P_vector
 
 
 def test_EllipticOperations():
     secp256k1 = EllipticCurve(p,a,b)
-    pubKeyTest = secp256k1.scalar_multiply(privKey, origin_G)
-    assert pubKeyTest[0] == pubKey[0]
+    n_times_P = secp256k1.scalar_multiply(n, origin_G)
+    assert n_times_P[0] == n_times_P_vector[0]
+    assert n_times_P[1] == n_times_P_vector[1]
