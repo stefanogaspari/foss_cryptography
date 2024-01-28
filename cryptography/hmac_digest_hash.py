3b026a024ed00c7992b98c18fdc07421aa1da19d
stefanogaspari <stefano@stefanogaspari.com>
2024-01-28 11:37:51 +0300

    Modify hash function: bytes -> bytes

--- cryptography/hmac_digest_hash.py
+++ cryptography/hmac_digest_hash.py
@@ -1,2 +1,9 @@
 import hashlib
-sha_256 = hashlib.sha256
+
+def hash(msg: bytes, hash_function: str) -> bytes:
+    hash = hashlib.new(hash_function)
+    hash.update(msg)
+    return hash.digest()
+
+if __name__ == "__main__":
+    print(hash(b'this is just a test', 'sha256'))
