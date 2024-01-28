e47aa86109ee26859554df0c8744fa373935d7ab
stefanogaspari <stefano@stefanogaspari.com>
2024-01-28 10:37:58 +0300

    Improve readibility of the module ellipticCurves.py

--- cryptography/ellipticCurves.py
+++ cryptography/ellipticCurves.py
@@ -1,13 +1,18 @@
+from typing import List
+
 class EllipticCurve:
-    def __init__(self, mod, a, b):
+
+    # Constructor: set an instance of a specific elliptic curve
+    def __init__(self, mod: int, a: int, b: int):
         self.mod = mod
         self.a = a
         self.b = b
         self.Infinity = None
         if (4 * pow(self.a,3) + 27 * pow(self.b,2)) == 0:
             raise ValueError("Value Error: elliptic curve with singularity")
+        
     # P + P = R
-    def double(self, P):
+    def double(self, P: List[int]) -> List[int]:
         if P is self.Infinity:
             return self.Infinity
         Px, Py = P
@@ -15,8 +20,9 @@ class EllipticCurve:
         Rx = (pow(lam, 2) - 2 * Px) % self.mod
         Ry = (lam * (Px - Rx) - Py) % self.mod
         return [Rx, Ry]
+    
     # P + Q = R
-    def add(self, P, Q):
+    def add(self, P: List[int], Q: List[int]) -> List[int]:
         if P is self.Infinity:
             return Q
         if Q is self.Infinity:
@@ -29,8 +35,9 @@ class EllipticCurve:
         Rx = (pow(lam, 2) - Px - Qx) % self.mod
         Ry = (lam * (Px - Rx) - Py) % self.mod
         return [Rx, Ry]
+    
     # n * P = R
-    def scalar_multiply(self, n, P):
+    def scalar_multiply(self, n: int, P: List[int]) -> List[int]:
         result = self.Infinity
         while n > 0:
             if n % 2 == 1:
